dbd07c0f0b7f6db40ed868e30cd15802fdf6c158
Bartosz Bialas <bartosz.bialas@fenira.com>
2016-02-27 10:17:46 +0100

    Simple request

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
+var config = require('./config'),
+    request = require('request');
+
+var makeRequest = function (callback) {
+    request(config.endpoint, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(JSON.parse(body)); // Show the HTML for the Google homepage.
+            callback();
+        } else {
+            throw response.statusCode;
+        }
+    })
+};
+
 module.exports = {
-   init: function() {
-       console.log('IGDB init!');
+   platforms: {
+       get: function(params, callback) {
+           makeRequest(callback);
+       },
+       meta: {
+           get: function(callback) {
+
+           }
+       },
+       games: function(params, callback) {
+
+       }
    }
 };
+
+
